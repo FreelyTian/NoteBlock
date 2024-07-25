9b63674947f8becdad6dafd6e9ea03e41303117d
freelytian <osk9ehdms01@gmail.com>
2024-07-25 18:17:30 -0300

    conversão de tipo refeita

--- lib/Bus.cpp
+++ lib/Bus.cpp
@@ -11,12 +11,12 @@ Bus::Bus() {
 Bus::~Bus() {
 }
 
-void Bus::write(uint16_t addr, uint8_t data) {
+void Bus::write(u16 addr, u8 data) {
     if (addr >= 0x0000 && addr <= 0xFFFF)
         ram[addr] = data;
 }
 
-uint8_t Bus::read(uint16_t addr, bool bReadOnly) {
+u8 Bus::read(u16 addr, bool bReadOnly) {
     if (addr >= 0x0000 && addr <= 0xFFFF)
         return ram[addr];
 
